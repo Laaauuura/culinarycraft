a4153b3c830b719a7fb239089e08d01fda5f1304
Laaauuura <104600276+Laaauuura@users.noreply.github.com>
2023-11-15 16:25:04 -0400

    spring inicio

---
+++ src/main/java/com/culinarycraft/culinarycraft/CulinarycraftbackApplication.java
@@ -0,0 +1,13 @@
+package com.culinarycraft.culinarycraft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CulinarycraftbackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CulinarycraftbackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/culinarycraft/culinarycraft/CulinarycraftbackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.culinarycraft.culinarycraft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CulinarycraftbackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
